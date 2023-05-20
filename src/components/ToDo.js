1f794b848225632a15f4fd0ee881437ade143939
RaviKanth-31 <74865487+RaviKanth-31@users.noreply.github.com>
2023-05-21 03:55:35 +0530

    Update ToDo.js
    Removed _blank

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -4,7 +4,7 @@ import {AiFillDelete} from "react-icons/ai"
 const ToDo = ({ text, ravi, prem, deleteToDo, updateP, updateR }) => {
     return (
         <div className="todo">
-            <div className="text"><a href={text} target='_blank' style={{color: "White"}}>{text}</a></div>
+            <div className="text"><a href={text} style={{color: "White"}}>{text}</a></div>
             <div className="icons">
                 <AiFillDelete className='icon' onClick={deleteToDo} />
                 <button onClick={() => updateP(prem?false:true)} style={{backgroundColor:prem?"Green":"Red", color:"White"}}>P</button>
