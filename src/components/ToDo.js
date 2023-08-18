d75b9cdd030299a91ef66350bfef3346d9f816e3
RaviKanth-31 <74865487+RaviKanth-31@users.noreply.github.com>
2023-08-18 23:59:04 +0530

    Add files via upload

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -3,12 +3,12 @@ import {AiFillDelete} from "react-icons/ai"
 
 const ToDo = ({ text, ravi, prem, deleteToDo, updateP, updateR }) => {
     return (
-        <div className="todo">
-            <div className="text"><a href={text} style={{color: "White"}} target='blank'>{text}</a></div>
+        <div className="todo" style={{backgroundColor:ravi?"Green":"Red", color:"White"}}>
+            <div className="text"><a href={text} target='_blank' style={{color: "White"}}>{text}</a></div>
             <div className="icons">
                 <AiFillDelete className='icon' onClick={deleteToDo} />
-                <button onClick={() => updateP(prem?false:true)} style={{backgroundColor:prem?"Green":"Red", color:"White"}}>P</button>
-                <button onClick={() => updateR(ravi?false:true)} style={{backgroundColor:ravi?"Green":"Red", color:"White"}}>R</button>
+                {/* <button onClick={() => updateP(prem?false:true)} style={{backgroundColor:prem?"Green":"Red", color:"White"}}>P</button> */}
+                <button onClick={() => updateR(ravi?false:true)} >R</button>
             </div>
         </div>
     )
